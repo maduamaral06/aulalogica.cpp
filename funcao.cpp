db4dc69b37f9cf65f0cc00636d288e436cac7634
maduamaral06 <maduamaral2006@gmail.com>
2025-06-13 20:38:14 -0300

    Exercicio Resolvido

---
+++ funcao.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// tipo_retorno + nome_função
+void boasVindas (){
+    cout << "\nBem vindos à aula de lógica de programação!";
+}
+
+int main(){
+
+//code
+
+//code
+
+boasVindas();
+
+//code
+
+boasVindas();
+
+//code
+
+
+return 0;
+
+
+}
